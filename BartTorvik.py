1c552c0331c0ee228bebc90d3bf67be020e2bdbb
ryanfdoc <ryanfdougherty@gmail.com>
2022-12-12 06:55:24 -0500

    commit

--- BartTorvik.py
+++ BartTorvik.py
@@ -21,35 +21,37 @@ for i in range(len(links)):
     links_values3.append(links[i].text)
 
 
-dft =pd.DataFrame(links_values3)
-
-dft =pd.DataFrame(np.array((links_values3)),
+dft = pd.DataFrame(links_values3)
+dft = pd.DataFrame(np.array((links_values3)),
                  columns=['team1'])
 
-
-links_values3 =np.array(links_values3)
+links_values3 = np.array(links_values3)
 
 print(dft.iloc[0:20])
 
 #creates torvik df
-dft2=dft.team1.str.split(expand=True)
-
-dft2=pd.DataFrame(dft.team1.str.split('\n',1).tolist(),
+dft2 = dft.team1.str.split(expand=True)
+print(dft2)
+dft2 = pd.DataFrame(dft.team1.str.split('\n',1).tolist(),
                          columns = ['Teams','PredictedScore'])
+print(dft2)
+Tovrik = dft2.iloc[2::4]
+print(Tovrik)
+Tovrik.columns = ['score1','score2']
+print(Tovrik)
 
-Tovrik=dft2.iloc[2::4]
-Tovrik.columns=['score1','score2']
-
-TovrikF=pd.DataFrame(Tovrik.score2.str.rsplit('-',n=2,expand=True))
-TovrikF.columns=['score1','score2']
-TovrikFinal2=pd.DataFrame(TovrikF.score2.str.rsplit('(',n=2,expand=True))
-Tovrikcombo=pd.concat([Tovrik, TovrikF.reindex(Tovrik.index)], axis=1).iloc[0:50]
-Tovrikx=pd.concat([Tovrikcombo, TovrikFinal2.reindex(Tovrikcombo.index)], axis=1).iloc[0:50]
-Tovrikx.columns=['Team','Pred','Score1','-','Score2','-']
+TovrikF = pd.DataFrame(Tovrik.score2.str.rsplit('-',n=2,expand=True))
+TovrikF.columns = ['score1','score2']
+TovrikFinal2 = pd.DataFrame(TovrikF.score2.str.rsplit('(',n=2,expand=True))
+Tovrikcombo = pd.concat([Tovrik, TovrikF.reindex(Tovrik.index)], axis=1).iloc[0:50]
+Tovrikx = pd.concat([Tovrikcombo, TovrikFinal2.reindex(Tovrikcombo.index)], axis=1).iloc[0:50]
+Tovrikx.columns = ['Team','Pred','Score1','-','Score2','-']
 
 
 
-TovFinal=Tovrikx
+TovFinal = Tovrikx
 TovFinal.drop(TovFinal.columns[[1,3,5]], axis=1, inplace=True)
 
-TovFinal.to_csv('BartTovik.csv')
+
+#TovFinal.to_csv('BartTovik.csv')
+#print(TovFinal)
